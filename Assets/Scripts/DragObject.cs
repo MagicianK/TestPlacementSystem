0ab3381b84dea0bfd5793c7c9cf3ea44a3b1586c
Magzhan Kaiyrboldy <justperson1938@gmail.com>
2023-02-21 13:12:53 +0600

    initial commit

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    private Vector3 offset;
+    private void OnMouseDown() {
+        offset = transform.position - PlacingSystem.GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag() {
+        Vector3 pos = PlacingSystem.GetMouseWorldPosition() + offset;
+        transform.position = PlacingSystem.current.SnapCoordinateToGrid(pos); 
+    }
+}
